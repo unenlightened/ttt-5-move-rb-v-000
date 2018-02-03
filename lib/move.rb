3e783b267e325318440a31d93fff8a9224f2c125
Chely Ho <adept_inept@yahoo.com>
2018-02-03 20:20:51 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,8 @@ end
 
 # code your input_to_index and move method here!
 
-def input_to_index(move)
-  move = gets.to_i
+def input_to_index(user_input)
+  converted_input = user_input.to_i
 end
 
 def move(a,b,c)
